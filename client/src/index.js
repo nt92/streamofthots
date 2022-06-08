d5e0c1a91e8d00e96eebdaea869b82766f6d0a95
Nikhil Thota <nthota92@gmail.com>
2022-06-08 10:50:41 -0500

    adding google analytics & new build

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {GA4React} from "ga-4-react";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,5 +11,14 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+try {
+    setTimeout(_ => {
+        const ga4react = new GA4React(process.env.REACT_APP_GA);
+        ga4react.initialize().catch(err => console.error(err));
+    }, 4000);
+} catch (err) {
+    console.error(err);
+}
 
 reportWebVitals();
